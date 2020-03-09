aad83ee2008737dc4d55cb43974d95add0768777
khoa <khoa.n997@gmail.com>
2020-03-09 11:35:57 -0700

    edited publisher code (controller.py)

--- teleop_ws/src/teleop_twist_joy/src/rpiHAT/controller.py
+++ teleop_ws/src/teleop_twist_joy/src/rpiHAT/controller.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 import rospy
+import  json
+from datetime import datetime
 import time
-from std_msgs.msg import Float32
+from std_msgs.msg import String
 from sensor_msgs.msg import Joy
-speedpub = rospy.Publisher('speed', Float32, queue_size=10)
-dirpub = rospy.Publisher('direction', Float32, queue_size=10)
+import pandas as pd
+pub = rospy.Publisher('cardata', String, queue_size=10)
+temp = {"speed":0.15,"direction":0.15}
+csv = {"speed":[],"direction":[],"time":[]}
+df = pd.DataFrame(csv)
 # from rpiHAT import ServoNT
 
 # direction =ServoNT(channel=1, freq=96.5)
@@ -28,19 +33,28 @@ def scale (unscaledNum, minAllowed, maxAllowed, min, max):
 
 def callback(data):
 
-    print(data)
+    #print(data)
     yd = data.axes[1]
     xd = data.axes[0]
     mv = data.buttons[7]
+    dataObj = {}
     if mv == 1:
-        speedpub.publish(0.13)
+        dataObj["speed"] = 0.13
     else:
-        speedpub.publish(0.15)
-    # print(data.buttons[8])
-    dirpub.publish(scale(xd,0.1,0.2,-1,1))
-    # print(scale(xd,0.1,0.2,-1,1))
-    # print(scale(yd,0.1,0.2,-1,1))
+        dataObj["speed"] = 0.15
+    dataObj["direction"] = scale(xd,0.1,0.2,-1,1) 
+    
+    if dataObj["speed"] != temp["speed"] or dataObj["direction"] != temp["direction"]:
+        #send data
+        pub.publish(json.dumps(dataObj))
+        global df
+        df = df.append({"speed":dataObj["speed"], "direction":dataObj["direction"],"time":str(datetime.now())},ignore_index=True)
+        df.to_csv("out.csv",index=False)
 
+
+
+    global temp # do not delete because it breaks :( everythigng
+    temp = dataObj
 
 def listener():
     rospy.init_node('motor_car', anonymous=True)
